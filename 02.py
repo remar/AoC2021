06f2b1ec94810325ca4f333938327379699f0e99
Andreas Remar <andreas.remar@gmail.com>
2021-12-03 17:15:39 +0100

    Solution day 2, part 2

--- 02.py
+++ 02.py
@@ -3,15 +3,17 @@ with open("02.txt") as f:
 
 x = 0
 y = 0
+aim = 0
 
 for command in data:
     (operation, amount) = command.split(" ")
     amount = int(amount)
     if operation == "forward":
         x += amount
+        y += aim * amount
     elif operation == "down":
-        y += amount
+        aim += amount
     elif operation == "up":
-        y -= amount
+        aim -= amount
 
 print(x*y)
